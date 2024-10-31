3e7a8e93c2a02f05113fbedbfb513f090763794b
leonardo mota <lmota5780@gmail.com>
2024-10-30 22:19:54 -0400

    creacion de proyecto

---
+++ src/components/principalPageComponents/principalPage.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+function PrincipalPage() {
+  return (
+    <Fragment>
+      <h1>esta es la pagina principal</h1> 
+    </Fragment>
+  )
+}
+
+export default PrincipalPage
