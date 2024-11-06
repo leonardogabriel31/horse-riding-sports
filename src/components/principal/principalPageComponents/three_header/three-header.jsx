6dfc83a913576a56e13cb18b3dec3ce25a8efea6
leonardo mota <lmota5780@gmail.com>
2024-11-06 14:02:45 -0400

    creacion tercer header y correcciones

---
+++ src/components/principal/principalPageComponents/three_header/three-header.jsx
@@ -0,0 +1,34 @@
+import { Box, Grid, Typography } from "@mui/material";
+import React, { Fragment } from "react";
+import Sponsors from "./three-header-components/Sponsors";
+import OurBestSelling from "./three-header-components/ourBestSelling";
+import GirlAndArticles from "./three-header-components/girlAndArticles";
+import RiderAreSaying from "./three-header-components/riderAreSaying";
+
+function ThreeHeader() {
+  return (
+    <Fragment>
+      <Box
+        component={"div"}
+        sx={{
+          background: "rgb(1, 19, 28)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          paddingTop: 15,
+          paddingBottom: "100px",
+        }}
+      >
+        <Sponsors />
+        <OurBestSelling />
+        <GirlAndArticles />
+        <RiderAreSaying />
+      </Box>
+    </Fragment>
+  );
+}
+
+export default ThreeHeader;
